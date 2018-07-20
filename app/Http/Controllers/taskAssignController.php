e69a6e8a99f9e1be5ea895072b3ac2466c2c7101
kamruzzaman <kamruzzaman@bookhint.com>
2018-07-20 16:18:10 +0600

    added for task role

--- app/Http/Controllers/taskAssignController.php
+++ app/Http/Controllers/taskAssignController.php
@@ -19,9 +19,23 @@ class taskAssignController extends Controller
     		$rolesList = DB::select('SELECT * FROM `mxp_role` WHERE `is_active` =1');
     	}
 
-    	$tasksList = mxpTask::where('status', 1);
+    	$tasksList = DB::select("SELECT * FROM `mxp_task` WHERE `status` = 1");
     	
-    	self::print_me($tasksList);
+    	return view('role_management.taskassign', compact('rolesList', 'tasksList'));
 
+    }
+    public function taskassignaction(Request $request)
+    {
+    	$company_id = $request->session()->get('company_id');
+
+    	if(!empty($company_id) && ($company_id != 0)){
+    		$rolesList = DB::select('SELECT * FROM `mxp_role` WHERE `company_id` = '.$company_id.' AND `is_active` =1');
+    	}else{
+    		$rolesList = DB::select('SELECT * FROM `mxp_role` WHERE `is_active` =1');
+    	}
+
+    	$tasksList = DB::select("SELECT * FROM `mxp_task` WHERE `status` = 1");
+    	
+    	self::print_me($request->get('task_1'));
     }
 }
